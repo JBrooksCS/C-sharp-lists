095eeaf600cd7ff8ec82eb3d2cf19b0da433525e
Jason <jason@Jasons-MacBook-Pro-2.local>
2019-07-08 14:26:51 -0500

    Second exercise completed and functioning

--- Program.cs
+++ Program.cs
@@ -7,26 +7,53 @@ namespace lists
     {
         static void Main(string[] args)
         {
-            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+
+            Console.WriteLine("----Results from Exercise 1----");
+
+
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
             planetList.Add("Jupiter");
             planetList.Add("Saturn");
 
-            List<string> lastPlanets = new List<string>(){"Neptune", "Uranus"};
+            List<string> lastPlanets = new List<string>() { "Neptune", "Uranus" };
 
             planetList.AddRange(lastPlanets);
             planetList.Insert(1, "Earth");
             planetList.Insert(1, "Venus");
             planetList.Add("Pluto");
 
-            
+
+            foreach (string item in planetList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("----Results from Exercise 2----");
+            //list of random numbers. Each number will be between 0 and 5.
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+                random.Next(6),
+            };
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if (numbers[i] == i){
+                    Console.WriteLine("numbers list contains " + i);
+                }
+                else {
+                    Console.WriteLine("numbers list does not contain " + i);
+                }
+            }
 
 
 
 
-        foreach (string item in planetList)
-        {
-            Console.WriteLine(item);
-        }
+
         }
     }
 }
